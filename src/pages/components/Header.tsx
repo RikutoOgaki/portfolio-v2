fb658d2c357a0d15102afa84db8df0cac9b853ca
Rikuto <orikutobasuke@gmail.com>
2023-04-14 11:31:54 +0900

    portfoliov2

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -3,7 +3,9 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 export function Header() {
     return (
         <>
-
+            <header>
+                <Box></Box>
+            </header>
         </>
     )
 }
