bd34f53cb2c1f5c282d02b94396a65fc5d07a1e2
Rikuto <orikutobasuke@gmail.com>
2023-04-18 10:08:32 +0900

    portfolipv2

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,10 +1,18 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export function Header() {
     return (
         <>
             <header>
-                <Box></Box>
+                <Flex
+                    width={'100%'}
+                    height={'100%'}
+                    justifyContent={'space-around'}>
+                    <Text>TOP</Text>
+                    <Text>ABOUT</Text>
+                    <Text>MtSkioo</Text>
+                </Flex>
             </header>
         </>
     )
