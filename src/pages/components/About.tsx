52a8d748f17f8b8ab380913866bce634bad5d95f
Rikuto <orikutobasuke@gmail.com>
2023-04-18 14:34:41 +0900

    PortFolio-v2

---
+++ src/pages/components/About.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+
+export default function About() {
+    return (
+        <>
+            <Flex w={'100%'} h={'60rem'} bg={'#fff'}>
+                <Flex flexDir={'column'}>
+                    <Text>-About Me-</Text>
+                    <Text>おおがき りくと</Text>
+                    <Text>大垣 陸斗</Text>
+                    <Text>2003年5月31日生まれ</Text>
+                    <Text>ECCコンピューター専門学校Webデザインコース</Text>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
