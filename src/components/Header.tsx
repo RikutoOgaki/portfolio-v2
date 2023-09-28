f3325a6d6c45df9c756d7804e49b4e0149bd5aa1
Rikuto <orikutobasuke@gmail.com>
2023-09-28 13:01:48 +0900

    Headerを完成させる

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
     return (
         <>
             <header className={style.head}>
-                <ul className={style.link}>
-                    <li className={style.Itemlink}><Link href={''}>Top</Link></li>
-                    <li className={style.Itemlink}><Link href={''}>About</Link></li>
-                    <li className={style.Itemlink}><Link href={''}>Skill</Link></li>
-                    <li className={style.Itemlink}><Link href={''}>Archive</Link></li>
+                <ul className={style.list}>
+                    <li className={style.itemList}><Link href={''}>Top</Link></li>
+                    <li className={style.itemList}><Link href={''}>About</Link></li>
+                    <li className={style.itemList}><Link href={''}>Skill</Link></li>
+                    <li className={style.itemList}><Link href={''}>Archive</Link></li>
                 </ul>
             </header>
         </>
