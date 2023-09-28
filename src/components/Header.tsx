bc042dfaa1235d0ae0f1c036fdc68fe9f2c3ab7f
Rikuto <orikutobasuke@gmail.com>
2023-09-28 12:23:12 +0900

    Headerを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
 import Link from 'next/link'
+import style from '@/styles/components/header.module.scss'
 
 export default function Header() {
     return (
         <>
-
+            <header className={style.head}>
+                <ul className={style.link}>
+                    <li className={style.Itemlink}><Link href={''}>Top</Link></li>
+                    <li className={style.Itemlink}><Link href={''}>About</Link></li>
+                    <li className={style.Itemlink}><Link href={''}>Skill</Link></li>
+                    <li className={style.Itemlink}><Link href={''}>Archive</Link></li>
+                </ul>
+            </header>
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head'
+import Header from '@/components/Header'
+
 export default function Portfolio() {
   return (
     <>
+      <Head>
+        <title>RikutoOgakiPortfolio</title>
+      </Head>
 
+      <Header />
     </>
   )
 }
