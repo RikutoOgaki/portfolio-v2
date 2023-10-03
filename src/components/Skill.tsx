04a7d726af3b9199f89cfe17953ada2a3d3bef3e
Rikuto <orikutobasuke@gmail.com>
2023-10-03 10:33:19 +0900

    skillの編集開始

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,7 +1,46 @@
-import { } from 'react-icons'
+import {
+    SiReact,
+    SiHtml5,
+    SiCss3,
+    SiSass,
+    SiJavascript,
+    SiTypescript,
+    SiChakraui,
+
+    SiAdobephotoshop,
+    SiAdobeillustrator,
+    SiAdobepremierepro,
+
+} from 'react-icons/si'
+import style from '@/styles/components/skill.module.scss'
 
 export default function Skill() {
     return (
-        <></>
+        <>
+            <div className={style.wrap}>
+                <div className={style.skillHead}>
+                    <h2>Skill</h2>
+                </div>
+                <div className={style.skillBox}>
+                    <div>
+                        <h3></h3>
+                        <div className={style.skillSubBox}>
+
+                        </div>
+                    </div>
+                    <div>
+                        <h3></h3>
+                        <div className={style.skillSubBox}>
+
+                        </div>
+                    </div>
+                    <div>
+                        <h3></h3>
+                        <div className={style.skillSubBox}>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
