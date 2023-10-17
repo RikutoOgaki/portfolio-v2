8bcc973e64d78cbefbf5b1b46c84b22221768a4f
Rikuto <orikutobasuke@gmail.com>
2023-10-17 12:25:03 +0900

    背景設定

--- src/components/common/MainView.tsx
+++ src/components/common/MainView.tsx
@@ -4,7 +4,7 @@ export function MainView() {
     return (
         <>
             <Flex w={'100%'} h={'100vh'} justifyContent={'center'} alignItems={'center'}>
-                <Flex w={{ base: '30rem', lg: '40rem' }} h={{ base: '15rem', lg: '20rem' }} bg={'blackAlpha.400'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} zIndex={999}>
+                <Flex w={{ base: '30rem', lg: '40rem' }} h={{ base: '15rem', lg: '20rem' }} bg={'blackAlpha.500'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} zIndex={999}>
                     <Text
                         color={'#ffffff'}
                         fontSize={{ base: '2xl', lg: '3xl' }}
