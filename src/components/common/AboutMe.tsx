5f0f12d501f70bb269d87b4259e3aaeefdd2b701
Rikuto <orikutobasuke@gmail.com>
2023-10-17 12:20:06 +0900

    AboutMe追加、レスポンシブテスト対応

---
+++ src/components/common/AboutMe.tsx
@@ -0,0 +1,21 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+
+export function AboutMe() {
+    return (
+        <>
+            <Flex
+                w={'100%'}
+                h={'100vh'}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                <Flex>
+                    <Box></Box>
+                    <Box>
+
+                    </Box>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
--- src/components/common/MainView.tsx
+++ src/components/common/MainView.tsx
@@ -4,18 +4,18 @@ export function MainView() {
     return (
         <>
             <Flex w={'100%'} h={'100vh'} justifyContent={'center'} alignItems={'center'}>
-                <Flex w={'40rem'} h={'20rem'} bg={'blackAlpha.400'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} zIndex={999}>
+                <Flex w={{ base: '30rem', lg: '40rem' }} h={{ base: '15rem', lg: '20rem' }} bg={'blackAlpha.400'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} zIndex={999}>
                     <Text
                         color={'#ffffff'}
-                        fontSize={'3xl'}
+                        fontSize={{ base: '2xl', lg: '3xl' }}
                     >Hi</Text>
                     <Text
                         color={'#ffffff'}
-                        fontSize={'7xl'}
+                        fontSize={{ base: '5xl', lg: '7xl' }}
                     >RikutoOgaki</Text>
                     <Text
                         color={'#ffffff'}
-                        fontSize={'3xl'}
+                        fontSize={{ base: '2xl', lg: '3xl' }}
                     >Front End Engineer</Text>
                 </Flex>
             </Flex>
