7959ad0d26a9ddd2c3a2dd6671d8cb3ea5f156e5
Rikuto <orikutobasuke@gmail.com>
2024-01-12 15:36:14 +0900

    制作開始

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import styles from './page.module.scss'
+import { MainView } from '@/components/MainView'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <div>
+        <MainView />
+      </div>
+    </>
   )
 }
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import style from '@/styles/components/aboutme.module.scss'
+
+export function AboutMe() {
+    return (
+        <>
+            <div className={style.aboutMeWrap}>
+
+            </div>
+        </>
+    )
+}
