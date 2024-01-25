b6de75a72d1f059ade61aa9454b306a530c662d3
Rikuto <orikutobasuke@gmail.com>
2024-01-25 23:50:07 +0900

    firstView8割完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
-import styles from "./page.module.css";
+import style from "./page.module.scss";
+
+import { Cube } from '@/components/common/Cube'
+
 
 export default function Home() {
+
+  const linkArray = [
+    {
+      id: 1,
+      linkIcon: '',
+      link: ''
+    },
+    {
+      id: 2,
+      linkIcon: '',
+      link: ''
+    },
+    {
+      id: 3,
+      linkIcon: '',
+      link: ''
+    }
+  ]
+
   return (
     <>
-      <h1>test</h1>
+      <div className={style.portfolioWrap}>
+        <div className={style.firstViewBox}>
+          <div className={style.textBox}>
+            <p>Hi</p>
+            <h2>Rikuto Ogaki</h2>
+            <p>Full Stack Engineer</p>
+            <div className={style.linkBox}>
+              {/* {
+                linkArray.map((v, idx) =>
+                  <div
+                    onClick={() => location.href = `${v.link}`}
+                  >
+                    {v.linkIcon}
+                  </div>
+                )
+              } */}
+            </div>
+          </div>
+          <div className={style.cubeBox}>
+            <Cube />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
