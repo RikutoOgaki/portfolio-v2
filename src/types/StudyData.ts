2e53042ff557eb1eefd61cf2d3ce5345fad480ef
Rikuto <orikutobasuke@gmail.com>
2024-01-30 11:36:46 +0900

    データの編集

--- src/types/StudyData.ts
+++ src/types/StudyData.ts
@@ -4,7 +4,7 @@ export const StudyData = [
         id: 1,
         workName: 'カウントアップタイマー',
         workTime: '2年後期',
-        workSkill: ['', 'CSS', 'jquary'],
+        workSkill: ['Next.js(React+TypeScript)', , 'SCSS'],
         workLink: 'https://timer-pied.vercel.app/',
         imgLink: '/img/timer.jpg',
         overview: '大会に向けて時間経過を知るためにNext.jsの練習で制作したカウントアップタイマー',
